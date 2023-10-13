e8fc00f31f604f91be386df5521106d9d30b5440
BlueFin605 <bluefin605@gmail.com>
2023-10-13 14:43:12 +1300

    basic angular electron app

---
+++ App/RestEasy/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+// include the Node.js 'path' module at the top of your file
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadFile('dist/rest-easy/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
