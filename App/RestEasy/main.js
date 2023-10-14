396254ac62675dba9372fd6cd3b19684763ba820
BlueFin605 <mitchelldean07@gmail.com>
2023-10-15 11:47:15 +1300

    Divedeeper (#2)
    * add router, might make things easier going forward
    
    * Add some new services, ready to be implemented
    
    * image browser but is not working
    
    * got image browser working
    
    * restructured files

--- App/RestEasy/main.js
+++ App/RestEasy/main.js
@@ -1,14 +1,25 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 // include the Node.js 'path' module at the top of your file
 const path = require('node:path')
+const fs = require('fs');
+const url = require('url');
+
+let win;
 
 const createWindow = () => {
-    const win = new BrowserWindow({
+    console.log('createWindow');
+    win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
-    win.loadFile('dist/rest-easy/index.html')
+    win.webContents.openDevTools();
+
+    win.loadFile('dist/rest-easy/index.html');
+    console.log('createWindow, done');
 }
 
 app.whenReady().then(() => {
@@ -22,3 +33,46 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
+
+
+function getImages() {
+    console.log('getImages');
+    const cwd = process.cwd();
+    fs.readdir('.', {withFileTypes: true}, (err, files) => {
+        if (!err) {
+            const re = /(?:\.([^.]+))?$/;
+            const images = files
+              .filter(file => file.isFile() && ['jpg', 'png'].includes(re.exec(file.name)[1]))
+              .map(file => `file://${cwd}/${file.name}`);
+            win.webContents.send("getImagesResponse", images);
+        }
+    });
+  }
+  
+  function isRoot() {
+      return path.parse(process.cwd()).root == process.cwd();
+  }
+  
+  function getDirectory() {
+    console.log('getDirectory');
+    fs.readdir('.', {withFileTypes: true}, (err, files) => {
+        if (!err) {
+            const directories = files
+              .filter(file => file.isDirectory())
+              .map(file => file.name);
+            if (!isRoot()) {
+                directories.unshift('..');
+            }
+            console.log(`directories:[${JSON.stringify(directories)}]`);
+            win.webContents.send("getDirectoryResponse", directories);
+        }
+    });
+    console.log('getDirectory, done');
+  }
+  
+  ipcMain.on("navigateDirectory", (event, path) => {
+    process.chdir(path);
+    getImages();
+    getDirectory();
+  });
+  
--- App/RestEasy/src/app/app.component.ts
+++ App/RestEasy/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ImagesService } from './services/image-services/images.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'RestEasy';
+
+  constructor(private imageService: ImagesService) {}
+
+  ngOnInit(): void {
+    console.log('navigate root dir');
+    this.imageService.navigateDirectory('.');
+  }
 }
