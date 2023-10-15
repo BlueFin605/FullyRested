1f55d6ac0f94c9798a1eb7659c3237e380598f9b
BlueFin605 <bluefin605@gmail.com>
2023-10-15 15:23:33 +1300

    add new component

--- App/RestEasy/src/app/app.module.ts
+++ App/RestEasy/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserComponent } from './components/browser/browser.component';
+import { RestActionComponent } from './components/rest-action/rest-action.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BrowserComponent
+    BrowserComponent,
+    RestActionComponent
   ],
   imports: [
     BrowserModule,
---
+++ App/RestEasy/src/app/components/rest-action/rest-action.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rest-action',
+  templateUrl: './rest-action.component.html',
+  styleUrls: ['./rest-action.component.css']
+})
+export class RestActionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
