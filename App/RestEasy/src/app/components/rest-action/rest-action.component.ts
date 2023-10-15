0bd058a59cc18c739a2f8e1540f12200ace4a4f4
BlueFin605 <bluefin605@gmail.com>
2023-10-15 19:19:47 +1300

    tidied up form pieces

--- App/RestEasy/src/app/components/rest-action/rest-action.component.ts
+++ App/RestEasy/src/app/components/rest-action/rest-action.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class RestActionComponent implements OnInit {
   verb = 'GET';
   protocol="HTTPS";
+  url = 'jsonplaceholder.typicode.com/todos/1';  //see https://jsonplaceholder.typicode.com/
 
   constructor() { }
 
