46ca211daf38fb896b4d0b9b7f1628bcaa0b83d9
BlueFin605 <bluefin605@gmail.com>
2023-10-24 07:12:23 +1300

    improved serializer

--- App/RestEasy/src/app/services/CustomUrlSerializer.ts
+++ App/RestEasy/src/app/services/CustomUrlSerializer.ts
@@ -1,4 +1,4 @@
-import {UrlSerializer, UrlTree, DefaultUrlSerializer} from '@angular/router';
+import { UrlSerializer, UrlTree, DefaultUrlSerializer } from '@angular/router';
 
 export class CustomUrlSerializer implements UrlSerializer {
     parse(url: any): UrlTree {
@@ -11,6 +11,16 @@ export class CustomUrlSerializer implements UrlSerializer {
             path = dus.serialize(tree);
         // use your regex to replace as per your requirement.
         // console.log(`serialize:[${JSON.stringify(path)}]`);
-        return path.replace(/%2F/g,'/').replace(/%20/g,' ');
+        // return path.replace(/%2F/g,'/').replace(/%20/g,' ');
+        return path
+            .replace(/%40/gi, '@')
+            .replace(/%3A/gi, ':')
+            .replace(/%24/gi, '$')
+            .replace(/%2C/gi, ',')
+            .replace(/%3B/gi, ';')
+            .replace(/%20/gi, '+')
+            .replace(/%3D/gi, '=')
+            .replace(/%3F/gi, '?')
+            .replace(/%2F/gi, '/')
     }
 }
