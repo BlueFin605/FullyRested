bd4350f9e4fae97e1a106696fa0de57ca5849564
BlueFin605 <bluefin605@gmail.com>
2023-10-25 05:57:11 +1300

    electron version was not working

--- App/RestEasy/main.js
+++ App/RestEasy/main.js
@@ -55,7 +55,7 @@ app.whenReady().then(() => {
                      statusText: response.statusText, 
                      headers: response.headers, 
                      headersSent: response.request._headers,
-                     data: response.data };
+                     body: response.data };
         }
         catch (error) {
             console.log(`Exception:[${JSON.stringify(error)}]`)
