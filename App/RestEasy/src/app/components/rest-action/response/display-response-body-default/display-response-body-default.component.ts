2ecd09f259b15c314383ea8126e086a7b14b1e73
BlueFin605 <bluefin605@gmail.com>
2023-10-27 23:12:06 +1300

    default view should convert to text

--- App/RestEasy/src/app/components/rest-action/response/display-response-body-default/display-response-body-default.component.ts
+++ App/RestEasy/src/app/components/rest-action/response/display-response-body-default/display-response-body-default.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RestActionResultBody } from 'src/app/services/execute-rest-calls/execute-rest-calls.service';
+import { ContentTypeHelperService } from 'src/app/services/content-type-helper/content-type-helper.service';
 
 @Component({
   selector: 'app-display-response-body-default',
@@ -7,10 +8,19 @@ import { RestActionResultBody } from 'src/app/services/execute-rest-calls/execut
   styleUrls: ['./display-response-body-default.component.css']
 })
 export class DisplayResponseBodyDefaultComponent implements OnInit {
+  rawData: string = '';
+  
   @Input()
-  body: RestActionResultBody | undefined;
+  set body(body: RestActionResultBody | undefined) {
+    if (body == undefined) {
+      this.rawData = '';
+      return;
+    }
 
-  constructor() { 
+     this.rawData = this.contentTypeHelper.convertArrayBufferToString(body.contentType, body.body);
+  }
+
+  constructor(private contentTypeHelper: ContentTypeHelperService) { 
   }
 
   ngOnInit(): void {
