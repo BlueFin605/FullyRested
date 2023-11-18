a9e5717fd54acd5448981db9f8507e2c6f59fccb
BlueFin605 <bluefin605@gmail.com>
2023-11-18 17:13:12 +1300

    open existing tab if file already opened

--- App/RestEasy/src/app/components/open-actions/open-actions.component.ts
+++ App/RestEasy/src/app/components/open-actions/open-actions.component.ts
@@ -152,9 +152,12 @@ export class OpenActionsComponent implements OnInit {
 
   loadAction($event: string) {
     console.log(`loadAction:[${$event}]`);
-    if (this.currentSession().actions.some(a => a.fullFilename == $event))
+    var existingTab = this.currentSession().actions.findIndex(a => a.fullFilename == $event);
+    if (existingTab != -1) {
+      this.tabs.selectedIndex = existingTab;
        return;
-      
+    }
+
     this.repo.loadRequest($event).then(a => {
       var newAction: LocalRestAction = {action: a, dirty: false, fullFilename: $event}; 
       this.currentSession().actions.push(newAction);
